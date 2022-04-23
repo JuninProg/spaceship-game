29cc4bff6920eaed11972143c6b535f1352c65b4
JuninProg <jose.lima.jjunior@gmail.com>
2022-04-23 13:57:36 -0300

    feat: add instructions section in Screen component

---
+++ src/@types/screen.d.ts
@@ -0,0 +1,1 @@
+export type ScreenName = 'rank' | 'instructions' | 'play';
--- src/components/Screen.vue
+++ src/components/Screen.vue
@@ -2,92 +2,23 @@
 import { ref } from 'vue';
 import Game from './game/Game.vue';
 import { usePlayerStore } from '../stores/player';
+import type { ScreenName } from '../@types/screen';
 
 const playerStore = usePlayerStore();
 
 const players = playerStore.players;
 
-playerStore.addPlayer({
-  name: 'JFL',
-  score: 123,
-});
-playerStore.addPlayer({
-  name: 'TEST',
-  score: 134123,
-});
-playerStore.addPlayer({
-  name: 'JFL',
-  score: 123,
-});
-playerStore.addPlayer({
-  name: 'TEST',
-  score: 134123,
-});
-playerStore.addPlayer({
-  name: 'JFL',
-  score: 123,
-});
-playerStore.addPlayer({
-  name: 'TEST',
-  score: 134123,
-});
-playerStore.addPlayer({
-  name: 'JFL',
-  score: 123,
-});
-playerStore.addPlayer({
-  name: 'TEST',
-  score: 134123,
-});
-playerStore.addPlayer({
-  name: 'JFL',
-  score: 123,
-});
-playerStore.addPlayer({
-  name: 'TEST',
-  score: 134123,
-});
-playerStore.addPlayer({
-  name: 'JFL',
-  score: 123,
-});
-playerStore.addPlayer({
-  name: 'TEST',
-  score: 134123,
-});
-playerStore.addPlayer({
-  name: 'TEST',
-  score: 134123,
-});
-playerStore.addPlayer({
-  name: 'JFL',
-  score: 123,
-});
-playerStore.addPlayer({
-  name: 'TEST',
-  score: 134123,
-});
+const screens = ref<ScreenName>('rank');
 
-playerStore.addPlayer({
-  name: 'TEST',
-  score: 134123,
-});
-playerStore.addPlayer({
-  name: 'JFL',
-  score: 123,
-});
-
-const screens = ref<'rank' | 'how_to_play' | 'before_play' | 'play'>('rank');
-
-function start() {
-  screens.value = 'play';
+function alterScreen(name: ScreenName) {
+  screens.value = name;
 }
 </script>
 
 <template>
   <div v-if="screens === 'rank'" class="rank">
     <div class="rank__title">
-      <p>TOP PLAYERS</p>
+      <p class="title">TOP PLAYERS</p>
     </div>
     <div class="rank__container">
       <div class="rank__row">
@@ -107,8 +38,28 @@ function start() {
       </div>
     </div>
     <div class="rank__buttons">
-      <button @click="start">JOGAR</button>
-      <button>COMO JOGAR</button>
+      <button @click="alterScreen('play')">START</button>
+      <button @click="alterScreen('instructions')">HOW TO PLAY</button>
+    </div>
+  </div>
+  <div v-else-if="screens === 'instructions'" class="instructions">
+    <div class="instructions__title">
+      <p class="title">HOW TO PLAY</p>
+    </div>
+    <div class="instructions__container">
+      <ol>
+        <li>Defeat as many asteroids as you can.</li>
+        <li>If an asteroid collides with the spaceship you lose.</li>
+        <li>The controls are:</li>
+        <ul>
+          <li>Use "W" to shoot.</li>
+          <li>Use "D" to move right.</li>
+          <li>Use "A" to move left.</li>
+        </ul>
+      </ol>
+    </div>
+    <div class="instructions__button">
+      <button @click="alterScreen('rank')">BACK</button>
     </div>
   </div>
   <Game v-else-if="screens === 'play'" />
@@ -118,6 +69,10 @@ function start() {
 p {
   margin: 0;
   width: 100%;
+}
+
+.title {
+  font-size: 24px;
 }
 
 .rank {
@@ -132,7 +87,6 @@ p {
   height: 10%;
   display: flex;
   align-items: center;
-  font-size: 24px;
 }
 
 .rank__buttons {
@@ -165,5 +119,32 @@ button:hover {
   flex-flow: row nowrap;
   justify-content: space-between;
   text-align: center;
+}
+.instructions {
+  background-color: black;
+  color: white;
+  display: flex;
+  flex-flow: column nowrap;
+  align-items: center;
+}
+
+.instructions__title {
+  height: 10%;
+  display: flex;
+  align-items: center;
+}
+
+.instructions__container {
+  height: 70%;
+  width: 80%;
+  display: flex;
+  align-items: center;
+  font-size: 20px;
+}
+
+.instructions__button {
+  height: 20%;
+  display: flex;
+  align-items: center;
 }
 </style>
